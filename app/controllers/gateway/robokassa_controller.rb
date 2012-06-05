b4c0daeb198320bfb8215113d9c69473fb8470f5
puffy <me@puffy.name>
2012-06-05 17:13:59 +0400

    Use Enumerable#detect instead of ActiveRecord::FinderMethods#find

--- app/controllers/gateway/robokassa_controller.rb
+++ app/controllers/gateway/robokassa_controller.rb
@@ -5,7 +5,7 @@ class Gateway::RobokassaController < Spree::BaseController
   
   def show
     @order =  Order.find(params[:order_id])
-    @gateway = @order.available_payment_methods.find{|x| x.id == params[:gateway_id].to_i }
+    @gateway = @order.available_payment_methods.detect{|x| x.id == params[:gateway_id].to_i }
 
     if @order.blank? || @gateway.blank?
       flash[:error] = I18n.t("invalid_arguments")
